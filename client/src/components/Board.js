b6f39a96166f5daf8365a90e09b56ddd920b80d5
Micah Caffey <mrcaffey@gmail.com>
2018-10-16 11:22:21 -0600

    updated board.js

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,1 +1,27 @@
-export default () => null
+import React from 'react'
+import { Grid, Button, Divider } from 'semantic-ui-react'
+import Dice from './Dice'
+
+const Board = ({ roll, dice, rollDice }) => {
+  const maxRoll = roll === 3
+  const disabled = maxRoll ? { disabled: true } : {}
+  return (
+  <Grid>
+    <Grid.Row>
+      <Button
+        fluid
+        onClick={rollDice}
+        {...disabled}
+        >
+        Roll
+        </Button>
+        <Grid.Column width={16}>
+          <Divider hidden />
+        </Grid.Column>
+        { roll > 0 && dice.map( (d,i) => <Dice key={i} value={d} /> ) }
+    </Grid.Row>
+  </Grid>
+  )
+}
+
+export default Board
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,1 +1,53 @@
-export default () => null
+import React from 'react'
+import { Grid, GridRow } from 'semantic-ui-react'
+import Board from './Board'
+import ScoreCard from './ScoreCard'
+import styled from 'styled-components'
+
+
+const FullHeight = styled(Grid.Column)`
+  height: 100vh;
+`
+const BoardContainer = styled(FullHeight)`
+  background-color: #AAFFAA;
+`
+const ScoreContainer = styled(FullHeight)`
+  background-color: #9370DB;
+`
+
+class Game extends React.Component{
+  state = { roll: 0, keep: [], dice: [...new Array(5)] }
+
+  rollDice = () => {
+    const dice = this.state.dice.map( (el, i) => {
+      return Math.floor(Math.random() * 6) + 1
+    })
+
+    this.setState( state => {
+      return { dice, roll: state.roll + 1 }
+      })
+  }
+
+  render() {
+    const { roll, dice } = this.state
+
+    return (
+    <Grid>
+      <GridRow>
+       <BoardContainer width={10}>
+        <Board
+        roll = {roll}
+        dice = {dice}
+        rollDice = {this.rollDice}
+        />
+       </BoardContainer>
+        <ScoreContainer>
+        <ScoreCard />
+      </ScoreContainer>
+    </GridRow>
+  </Grid>
+    )
+  }
+}
+
+export default Game

78a777c8d74600d6e475ed98c1cfa3e2b8900cc6
Micah Caffey <mrcaffey@gmail.com>
2018-10-17 12:21:19 -0600

    updated scoreRow

--- client/src/reducers/currentGame.js
+++ client/src/reducers/currentGame.js
@@ -1,7 +1,21 @@
-import { bindActionCreators } from "redux";
-
 const ROLL_DICE = 'ROLL_DICE'
 const TOGGLE_KEPT = 'TOGGLE_KEPT'
+
+const scores = [
+  { section: 'upper', name: 'Ones', score: null, value: 1 },
+  { section: 'upper', name: 'Twos', score: null, value: 2 },
+  { section: 'upper', name: 'Threes', score: null, value: 3 },
+  { section: 'upper', name: 'Fours', score: null, value: 4 },
+  { section: 'upper', name: 'Fives', score: null, value: 5 },
+  { section: 'upper', name: 'Sixes', score: null, value: 6 },
+  { section: 'lower', name: 'Three Of A Kind', score: null, addAll: true },
+  { section: 'lower', name: 'Four Of A Kind', score: null, addAll: true },
+  { section: 'lower', name: 'Full House', score: null },
+  { section: 'lower', name: 'Low Straight', score: null },
+  { section: 'lower', name: 'High Straight', score: null },
+  { section: 'lower', name: 'Yahtzee', score: null },
+  { section: 'lower', name: 'Chance', score: null, addAll: true }
+];
 
 export const rollDice = () => {
   return (dispatch, getState) => {
@@ -23,8 +37,8 @@ export const toggleKept = (i) => {
     let updated
 
     if (keep.includes(i))
-      updated = keep.filter( d => d !== i)
-      else
+      updated = keep.filter( d => d !== i )
+    else
       updated = [...keep, i]
 
     dispatch({ type: TOGGLE_KEPT, keep: updated })
@@ -36,9 +50,10 @@ export default (
     roll: 0,
     dice: [...new Array(5)],
     keep: [],
+    scores,
   },
   action
-  ) => {
+) => {
   switch(action.type) {
     case ROLL_DICE:
       return {
@@ -46,12 +61,12 @@ export default (
         dice: action.dice,
         roll: state.roll + 1
       }
-    case TOGGLE_KEPT:
+    case TOGGLE_KEPT: 
       return {
         ...state,
         keep: action.keep
       }
-    default: 
+    default:
       return state
   }
 }

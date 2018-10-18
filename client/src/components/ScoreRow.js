cc8eb2b7fddb3824a1ed28a32eb332cc4cf84d55
Micah Caffey <mrcaffey@gmail.com>
2018-10-18 10:39:21 -0600

    updated functions

--- client/src/components/ScoreRow.js
+++ client/src/components/ScoreRow.js
@@ -2,12 +2,38 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { List, Header } from 'semantic-ui-react'
 import styled from 'styled-components'
+import { updateScores, resetRoll } from '../reducers/currentGame'
+import {
+  singles,
+  addAllDice,
+  staticScore,
+} from '../utils/scoringEngine'
 
 const Pointer = styled(List.Icon)`
   cursor: pointer;
 `
 
 class ScoreRow extends React.Component {
+  updateScore = (key) => {
+    const { currentGame: { dice, scores }, dispatch } = this.props;
+    const entry = scores.find( d => d.name === key )
+    dispatch(resetRoll())
+
+    if (entry.value)
+      entry.score = singles(entry.value, dice)
+    else if (entry.addAll)
+      entry.score = addAllDice(entry.name, dice)
+    else
+      entry.score = staticScore(entry.name, dice)
+
+      const newScores = scores.map( score => {
+        if (score.name === key) 
+        return entry
+        return score
+  })
+    dispatch(this.updateScores(newScores))
+}
+
   render() {
     const { name, score, currentGame: { roll }} = this.props
     return (
@@ -16,6 +42,7 @@ class ScoreRow extends React.Component {
             <Pointer
               name="check circle outline"
               color="green"
+              onClick={ roll !== 0 ? () => this.updateScore(name) : f => f }
             />
         }
         <List.Content>

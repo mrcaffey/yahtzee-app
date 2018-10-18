a43d46de84f45d55c3593914b9c1d2a31a95056e
Micah Caffey <mrcaffey@gmail.com>
2018-10-18 10:21:01 -0600

    defined scoringEngine functions

--- client/src/components/ScoreRow.js
+++ client/src/components/ScoreRow.js
@@ -5,31 +5,30 @@ import styled from 'styled-components'
 
 const Pointer = styled(List.Icon)`
   cursor: pointer;
-  `
+`
 
-  class ScoreRow extends React.Component {
-    render() {
-      const { name, score, currentGame: { roll }} = this.props
-      return (
-        <List.Item>
-          { score === null && 
+class ScoreRow extends React.Component {
+  render() {
+    const { name, score, currentGame: { roll }} = this.props
+    return (
+      <List.Item>
+        { score === null &&
             <Pointer
               name="check circle outline"
               color="green"
-              />
-          }
-
-          <List.Content>
-            <Header as="h4" floated="left">{score || 0}</Header>
-            <Header as="h5" floated="right">{name}</Header>
-          </List.Content>
-        </List.Item>
-      )
-    }
+            />
+        }
+        <List.Content>
+          <Header as="h4" floated="left">{score || 0}</Header>
+          <Header as="h5" floated="right">{name}</Header>
+        </List.Content>
+      </List.Item>
+    )
   }
+}
 
-  const mapStateToProps = (state) => {
-    return { currentGame: state.currentGame }
-  }
+const mapStateToProps = (state) => {
+  return { currentGame: state.currentGame }
+}
 
-  export default connect (mapStateToProps)(ScoreRow)
+export default connect(mapStateToProps)(ScoreRow)
